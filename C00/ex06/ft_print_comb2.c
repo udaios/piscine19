46ea7c6993552ea954178f7c420dc9cc60910224
Mikael Ruhl <mruhl@shi-r2-p3.s19.be>
2020-09-17 18:58:24 +0200

    putcomb2

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,54 +6,39 @@
 /*   By: mruhl <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 13:11:28 by mruhl             #+#    #+#             */
-/*   Updated: 2020/09/17 15:09:49 by mruhl            ###   ########.fr       */
+/*   Updated: 2020/09/17 18:56:31 by mruhl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_display(char a, char b, char c, char d)
+void	ft_display(char c)
 {
-	write(1, &a, 1);
-	write(1, &b, 1);
-	write(1, " ", 1);
 	write(1, &c, 1);
-	write(1, &d, 1);
-	write(1, ",", 1);
-	write(1, " ", 1);
 }
 
 void	ft_print_comb2(void)
 {
 	int a;
 	int b;
-	
+
 	a = 0;
 	b = 1;
 	while (a <= 98)
 	{
-		b = a + 1;
-		while (b <= 99)
+		while (b < 100)
 		{
-			if (a >= 10 && b >= 10)
-			{
-				ft_display(a / 10 + '0', a % 10, b / 10 + '0', b % 10);
-			}
-			else if (a < 10)
-			{
-				ft_display('0', a % 10, b / 10 + '0', b % 10);
-			}
-			else if (b < 10)
-			{
-				ft_display(a /10 + '0', a % 10, '0', b % 10);
-			}
-			else
-			{
-				ft_display('0', a % 10, '0', b % 10);
-			}
+			ft_display(a / 10 + '0');
+			ft_display(a % 10 + '0');
+			ft_display(' ');
+			ft_display(b / 10 + '0');
+			ft_display(b % 10 + '0');
+			if (a < 98)
+				write(1, ", ", 2);
 			b++;
 		}
 		a++;
+		b = a + 1;
 	}
 }
 
